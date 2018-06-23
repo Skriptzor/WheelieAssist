8c682e81904c739f216306f8124ac0a7e85a33b4
Josh <josh@192168012021.tpgi.com.au>
2018-06-23 15:26:13 +1000

    Added siq buttons to simulate ultrasonic detection. Cleaned up variable output for Bluetooth

--- Wheelie_Assist_V0.4/Wheelie_Assist_V0.4.ino
+++ Wheelie_Assist_V0.4/Wheelie_Assist_V0.4.ino
@@ -3,14 +3,21 @@
 #define YAxis 33
 #define SonarTrig 11
 #define SonarEcho 12
-#define Ver 0.4
+#define Ver 0.5
+#define Refresh 100
+
+
+//Temporary, until ultrasonic is fixed
+#define OverideON 4
+#define OverideOFF 2
+int WheelieOveride = 0;
 
 //These variable need to be 'SET' when the app is running.
 
 //Can be 'set' once rider is on the bike.
 int YAxisStart = 1770; //What is 'FLAT' for the Y Axis?
 
-//Can be set by lifting the forks as high as possible. How could we make this possible for just 1 person?
+//Can be set by lifting the forks as high as possible. How could we make this possible for just 1 person to set?
 int ForkHeight = 4; //Height of extended fork (in CM)
 
 //This can be calculated by YAxisStart+-75.
@@ -22,11 +29,14 @@ int highestWheelie = 0;
 long SonarDuration, SonarCm; //Variables for Sonar distance calculations
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(YAxis,INPUT);
+  pinMode(OverideON,INPUT);
+  pinMode(OverideOFF,INPUT);
   //pinMode(SonarTrig,OUTPUT);
   //pinMode(SonarEcho,INPUT);
   
-  Serial.begin(9600);
   Serial.print("System ready. Wheelie Assist V");
   Serial.println(Ver);
   delay(2000); 
@@ -36,20 +46,34 @@ void loop() {
   int Y = analogRead(YAxis);
   int angle = map(Y,YAxisStart,YAxisEnd,0,90);
 
+  int buttonStateON = digitalRead(OverideON);
+  int buttonStateOFF = digitalRead(OverideOFF);
+  
+  if(buttonStateON == 1) { //overide, make the system think we're doing a wheelie.
+    SonarCm = 5;
+    highestWheelie = 0; //Reset highest wheelie.
+    Serial.println("wheelie detected");
+  }
+  else if(buttonStateOFF == 1) { //override, make the system think the wheelie has ended.
+    SonarCm = 0;
+  }
+  
+  if(SonarCm < ForkHeight && highestWheelie > 0) {
+    Serial.print("Congrats on the wheelie! You got to ");
+    Serial.print(highestWheelie);
+    Serial.println("*");
+    highestWheelie = 0;
+  }
+
+
   //Check to see if bike is flat again (Use ultrasonic).
   //If bike is flat and there's a highest wheelie above 0, send data to bluetooth function by Martin
   //After bluetooth function, set highest wheelie to 0.
   
-  Serial.print("Y/Y*: ");
-  Serial.print(Y);
-  Serial.print("/");
-  Serial.print(angle);
-  Serial.print("*. Highest: ");
-  Serial.print(highestWheelie);
-  Serial.println("*");
-
-  if(angle > highestWheelie) { //record highest wheelie. When should this reset?
-    highestWheelie = angle;
+  if(SonarCm > ForkHeight) {
+    if(angle > highestWheelie) { //record highest wheelie. When should this reset?
+      highestWheelie = angle;
+    }
   }
   
   /*
@@ -64,7 +88,7 @@ void loop() {
   SonarDuration = pulseIn(SonarEcho, HIGH);
   SonarCm = (SonarDuration/2) / 29.1; //Convert sonar return duration into CM
 
-  if(SonarCm > ForkHeight)
+  
   */
-  delay(100);
+  delay(Refresh);
 }
